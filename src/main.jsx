828362193b8c068f0dd343b70fc658a8a5e64bda
Romylson <romylson98@gmail.com>
2026-09-06 23:46:19 -0300

    testa botao jogo

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
-import './index.css';
+import "./index.css";
 import "./i18n";
 import { ThemeProvider } from './context/ThemeContext.jsx';
 

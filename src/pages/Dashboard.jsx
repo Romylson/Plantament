e105ce027e1acc5ca7cf5c9b846388d8f9e3e11c
Romylson <romylson98@gmail.com>
2026-09-08 14:15:16 -0300

    ajustes de imagens de cerebro

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -9,7 +9,7 @@ import IntroBlock from "../components/IntroBlock";
 import logosParceiros from "../assets/parceiros-rodapé.png";
 import cerebroplanta from "../assets/cerebroplanta.png";
 import logoPlantamente from "../assets/plantamente.jpg";
-
+import cerebroroxo from "../assets/cerebroroxo.jpg"
 
 // Mapeamento dos 6 cards principais da tela
 const cardsPrincipais = [
@@ -17,7 +17,7 @@ const cardsPrincipais = [
     titulo: "Entenda o Alzheimer",
     descricao: "Causas, sintomas, estágios e avanços na pesquisa.",
     rota: "/alzheimer-dados",
-    img: "/assets/cerebroroxo.jpg",
+    img: cerebroroxo,
   },
   {
     titulo: "Explore a Farmacologia",
